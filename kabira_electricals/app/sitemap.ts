d6008f5ad6f517b0545dd7917464f9bb0775a2de
idris10215 <mohammedidris10215@gmail.com>
2026-09-03 09:43:19 +0530

    Update

--- kabira_electricals/app/sitemap.ts
+++ kabira_electricals/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next";
 import { servicesData } from "@/data/services";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://kabiraelectricals.com";
 
